e7178b983471e46f8aa5f5f289ca414a2c5445b3
alainpaluku <alainpaluku@proton.me>
2026-04-24 13:01:34 +0200

    fix: simplify Cloudflare config for Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,6 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough',
-    platformProxy: {
-      enabled: true
-    },
     prerenderEnvironment: 'node'
   }),
   server: {
